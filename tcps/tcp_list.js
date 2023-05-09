1e24f60de5e21e134cdaa21dbc59cb2bb2017004
chris <christopher.logston@sfdpw.org>
2023-05-08 21:54:58 -0700

    modified:   tcps/tcp_list.js

--- tcps/tcp_list.js
+++ tcps/tcp_list.js
@@ -17,10 +17,10 @@ var project_tcp_array = [
  'numb': '012', 'desc': 'SW: 46th from Vicente to Ulloa', 'rlvn': ['SW_08', 'SW_11', 'SW_12']
  }, 
 { 
- 'numb': '018', 'desc': 'PSP 1', 'rlvn': ['SW_01', 'SW_02', 'SW_03', 'SW_04', 'SW_05', 'SW_06', 'SW_07', 'SW_08']
+ 'numb': '018', 'desc': 'Storage Plan 1', 'rlvn': ['SW_01', 'SW_02', 'SW_03', 'SW_04', 'SW_05', 'SW_06', 'SW_07', 'SW_08']
  }, 
 { 
- 'numb': '019', 'desc': 'PSP 2', 'rlvn': ['SW_09', 'SW_10', 'SW_11', 'SW_12']
+ 'numb': '019', 'desc': 'Storage Plan 2', 'rlvn': ['SW_09', 'SW_10', 'SW_11', 'SW_12']
  }, 
 
 
