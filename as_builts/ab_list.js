befabd1e1230cadd55faec51f5d34829c7198e33
chris <christopher.logston@sfdpw.org>
2023-09-07 19:28:14 -0700

    renamed:    as_builts/1070I_PP_01_as_built_SW_01.pdf -> as_builts/1070I_PP01_as_built_SW_01.pdf 	renamed:    as_builts/1070I_PP_02_as_built_SW_01.pdf -> as_builts/1070I_PP02_as_built_SW_01.pdf 	renamed:    as_builts/1070I_PP_02_as_built_SW_02.pdf -> as_builts/1070I_PP02_as_built_SW_02.pdf 	new file:   as_builts/1070I_PP03_as_built_SW_01.pdf 	new file:   as_builts/1070I_PP03_as_built_SW_02.pdf 	new file:   as_builts/1070I_PP03_as_built_SW_03.pdf 	new file:   as_builts/1070I_PP03_as_built_SW_04.pdf 	new file:   as_builts/1070I_PP03_as_built_SW_05.pdf 	modified:   as_builts/ab_list.js

--- as_builts/ab_list.js
+++ as_builts/ab_list.js
@@ -1,7 +1,12 @@
 var json_ab_list = [
 
-  '1070I_PP_01_as_built_SW_01',
-  '1070I_PP_02_as_built_SW_01',
-  '1070I_PP_02_as_built_SW_02'
-
+  '1070I_PP01_as_built_SW_01',
+  '1070I_PP02_as_built_SW_01',
+  '1070I_PP02_as_built_SW_02',
+  '1070I_PP03_as_built_SW_01',
+  '1070I_PP03_as_built_SW_02',
+  '1070I_PP03_as_built_SW_03',
+  '1070I_PP03_as_built_SW_04',
+  '1070I_PP03_as_built_SW_05'
+  
 ]
