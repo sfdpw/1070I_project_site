4ce3276eb1ea226ebf038187cc842e4e28f0ac21
chris <christopher.logston@sfdpw.org>
2024-03-29 13:20:49 -0700

    new file:   as_builts/1070I_PP10_as_built_PR_01.pdf 	new file:   as_builts/1070I_PP10_as_built_PR_02.pdf 	new file:   as_builts/1070I_PP10_as_built_PR_03_a.pdf 	new file:   as_builts/1070I_PP10_as_built_PR_03_b.pdf 	new file:   as_builts/1070I_PP10_as_built_PR_04.pdf 	new file:   as_builts/1070I_PP10_as_built_SW_03_a.pdf 	new file:   as_builts/1070I_PP10_as_built_SW_03_b.pdf 	new file:   as_builts/1070I_PP10_as_built_SW_05.pdf 	new file:   as_builts/1070I_PP10_as_built_SW_06.pdf 	new file:   as_builts/1070I_PP10_as_built_SW_08_a.pdf 	new file:   as_builts/1070I_PP10_as_built_SW_08_b.pdf 	new file:   as_builts/1070I_PP10_as_built_SW_08_c.pdf 	new file:   as_builts/1070I_PP10_as_built_SW_09.pdf 	new file:   as_builts/1070I_PP10_as_built_SW_10.pdf 	new file:   as_builts/1070I_PP10_as_built_SW_11.pdf 	modified:   as_builts/ab_list.js

--- as_builts/ab_list.js
+++ as_builts/ab_list.js
@@ -50,10 +50,27 @@ var json_ab_list = [
   '1070I_PP09_as_built_SW_11',
   '1070I_PP09_as_built_SW_12',
   '1070I_PP10_as_built_CR_05',
+  '1070I_PP10_as_built_PR_01',
+  '1070I_PP10_as_built_PR_02',
+  '1070I_PP10_as_built_PR_03_a',
+  '1070I_PP10_as_built_PR_03_b',
+  '1070I_PP10_as_built_PR_04',
   '1070I_PP10_as_built_SW_01',
   '1070I_PP10_as_built_SW_02',
-  '1070I_PP10_as_built_SW_03',
+  '1070I_PP10_as_built_SW_03_a',
+  '1070I_PP10_as_built_SW_03_b',
   '1070I_PP10_as_built_SW_04',
+  '1070I_PP10_as_built_SW_05',
+  '1070I_PP10_as_built_SW_06',
   '1070I_PP10_as_built_SW_07',
-  '1070I_PP10_as_built_SW_08'
+  '1070I_PP10_as_built_SW_08_a',
+  '1070I_PP10_as_built_SW_08_b',
+  '1070I_PP10_as_built_SW_08_c',
+  '1070I_PP10_as_built_SW_09',
+  '1070I_PP10_as_built_SW_10',
+  '1070I_PP10_as_built_SW_11'
+
+
+
+
 ]
